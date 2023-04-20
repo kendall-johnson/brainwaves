c3a4fecc6f2d6ee425c95bbef8a1be08af579725
kendall-johnson <kdotjohnson2019@gmail.com>
2023-04-20 10:19:45 -0700

    Add Styling to Edit Form

--- src/components/MoodEditForm/MoodEditForm..jsx
+++ src/components/MoodEditForm/MoodEditForm..jsx
@@ -47,127 +47,190 @@ export default function MoodEditForm({ mood, onCancelEdit, onSaveEdit }) {
   };
 
   return (
-    <div>
-      <h2>Edit Mood</h2>
-      <label>
-        <span>Feeling:</span>
-        <select value={feeling} onChange={handleFeelingChange}>
-          <option value="Happy">Happy</option>
-          <option value="Sad">Sad</option>
-          <option value="Neutral">Neutral</option>
-          <option value="Mad">Mad</option>
-        </select>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+  <h2 className="text-3xl font-bold text-gray-900 mb-8">Edit Mood</h2>
+  <div className="bg-white rounded shadow-lg p-8 w-full md:w-3/4 lg:w-1/2">
+    <div className="mb-6">
+      <label className="text-gray-900 font-bold mb-2 block">
+        Feeling:
       </label>
-      <label>
-        <span>Emotions:</span>
-        <div>
-          <label>
-            <input
-              type="checkbox"
-              value="Joy"
-              name="emotion"
-              checked={emotions.includes('Joy')}
-              onChange={handleEmotionChange}
-            />
-            Joy
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              value="Anger"
-              name="emotion"
-              checked={emotions.includes('Anger')}
-              onChange={handleEmotionChange}
-            />
-            Anger
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              value="Fear"
-              name="emotion"
-              checked={emotions.includes('Fear')}
-              onChange={handleEmotionChange}
-            />
-            Fear
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              value="Sadness"
-              name="emotion"
-              checked={emotions.includes('Sadness')}
-              onChange={handleEmotionChange}
-            />
-            Sadness
-          </label>
-        </div>
-      </label>
-      <label>
-        <span>Triggers:</span>
-        <div>
-          <label>
-            <input
-              type="checkbox"
-              value="Family"
-              name="triggers"
-              checked={triggers.includes('Family')}
-              onChange={handleTriggerChange}
-            />
-            Family
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              value="Work"
-              name="triggers"
-              checked={triggers.includes('Work')}
-              onChange={handleTriggerChange}
-            />
-            Work
-            </label>
-      <label>
-        <input
-          type="checkbox"
-          value="School"
-          name="triggers"
-          checked={triggers.includes('School')}
-          onChange={handleTriggerChange}
-        />
-        School
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          value="Social Media"
-          name="triggers"
-          checked={triggers.includes('Social Media')}
-          onChange={handleTriggerChange}
-        />
-        Social Media
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          value="Breakup"
-          name="triggers"
-          checked={triggers.includes('Breakup')}
-          onChange={handleTriggerChange}
-        />
-        Breakup
-      </label>
+      <div className="relative inline-flex">
+  <svg
+    className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none"
+    viewBox="0 0 412 232"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.5 116C1.5 57.165 57.165 1.5 116 1.5H296C354.835 1.5 410.5 57.165 410.5 116V173.5"
+      stroke="#E5E7EB"
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+  <select
+    className="w-full appearance-none rounded-lg py-3 pl-10 pr-3 text-gray-900 font-semibold focus:outline-none focus:shadow-outline-blue focus:border-blue-300 bg-white border border-gray-400 hover:border-gray-500"
+    value={feeling}
+    onChange={handleFeelingChange}
+  >
+    <option value="Happy">Happy</option>
+    <option value="Sad">Sad</option>
+    <option value="Neutral">Neutral</option>
+    <option value="Mad">Mad</option>
+  </select>
+  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+    <svg
+      className="fill-current h-4 w-4"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+    >
+      <path
+        fillRule="evenodd"
+        d="M14.35 7.56a1 1 0 0 0-1.41 0L10 10.3 6.06 7.56a1 1 0 1 0-1.1 1.64l4 3.86a1 1 0 0 0 1.1 0l4-3.86a1 1 0 0 0 .3-.7 1 1 0 0 0-.3-.7z"
+      />
+    </svg>
+  </div>
+</div>
+
     </div>
-  </label>
-  <label>
-    <span>Reflection:</span>
-    <textarea
-      value={reflection}
-      onChange={handleReflectionChange}
-    ></textarea>
-  </label>
-  <button onClick={() => handleCancelEdit()}>Cancel</button>
-  <button onClick={() => handleSaveEdit({ feeling, emotions, triggers, reflection })}>
+    <div className="mb-6">
+      <label className="text-gray-900 font-bold mb-2 block">
+        Emotions:
+      </label>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="Joy"
+            name="emotion"
+            checked={emotions.includes('Joy')}
+            onChange={handleEmotionChange}
+            className="form-checkbox h-5 w-5 text-green-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">Joy</span>
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="Anger"
+            name="emotion"
+            checked={emotions.includes('Anger')}
+            onChange={handleEmotionChange}
+            className="form-checkbox h-5 w-5 text-red-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">Anger</span>
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="Fear"
+            name="emotion"
+            checked={emotions.includes('Fear')}
+            onChange={handleEmotionChange}
+            className="form-checkbox h-5 w-5 text-yellow-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">Fear</span>
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="Sadness"
+            name="emotion"
+            checked={emotions.includes('Sadness')}
+            onChange={handleEmotionChange}
+            className="form-checkbox h-5 w-5 text-blue-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">Sadness</span>
+        </label>
+      </div>
+    </div>
+    <div className="mb-6">
+      <label className="text-gray-900 font-bold mb-2 block">
+        Triggers:
+      </label>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="Family"
+            name="triggers"
+            checked={triggers.includes('Family')}
+            onChange={handleTriggerChange}
+            className="form-checkbox h-5 w-5 text-red-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">Family</span>
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="Work"
+            name="triggers"
+            checked={triggers.includes('Work')}
+            onChange={handleTriggerChange}
+            className="form-checkbox h-5 w-5 text-purple-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">Work</span>
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="School"
+            name="triggers"
+            checked={triggers.includes('School')}
+            onChange={handleTriggerChange}
+            className="form-checkbox h-5 w-5 text-blue-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">School</span>
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="Social Media"
+            name="triggers"
+            checked={triggers.includes('Social Media')}
+            onChange={handleTriggerChange}
+            className="form-checkbox h-5 w-5 text-green-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">
+            Social Media
+          </span>
+        </label>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            value="Breakup"
+            name="triggers"
+            checked={triggers.includes('Breakup')}
+            onChange={handleTriggerChange}
+            className="form-checkbox h-5 w-5 text-yellow-500"
+          />
+          <span className="ml-2 text-gray-900 font-medium">Breakup</span>
+        </label>
+      </div>
+    </div>
+    <div className="mb-6">
+      <label className="text-gray-900 font-bold mb-2 block">
+        Reflection:
+      </label>
+      <textarea
+        className="block w-full rounded-lg bg-gray-100 border-transparent text-gray-900 border-2 focus:border-gray-500 focus:bg-white focus:ring-0"
+        value={reflection}
+        onChange={handleReflectionChange}
+      />
+    </div>
+    <div className="flex justify-end">
+      <button
+        className="bg-gray-900 text-white py-2 px-4 rounded-full hover:bg-gray-700 transition-all duration-200"
+        onClick={() => handleCancelEdit()}
+      >
+        Cancel
+      </button>
+  <button className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-full ml-4 transition-all duration-200" onClick={() => handleSaveEdit({ feeling, emotions, triggers, reflection })}>
     Save
   </button>
 </div>
+</div>
+</div>
+
 )}
